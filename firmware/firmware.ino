4b3b2355a9f8f00914b731a95d46a3c2ea72b1b3
Akram Ali <AKstudios@users.noreply.github.com>
2019-12-22 23:00:47 -0600

    Update firmware.ino
    removed node ID from datapacket (gateway already has it in the encrypted packet, so it can retrieve it directly)

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,7 +1,7 @@
- // Element E500 v1.0.0 firmware
+ // Element E500 v1.1 firmware
 
 // Developed by AKstudios
-// Updated: 07/08/2019
+// Updated: 12/22/2019
 
 #include <RFM69.h>  //  https://github.com/LowPowerLab/RFM69
 #include <SPI.h>
@@ -11,9 +11,9 @@
 #include <avr/wdt.h>
 
 // define node parameters
-//#define NODEID              6 // must be unique for each node on same network (range up to 254, 0 is broadcast)
-uint16_t NODEID =             6; // same as above, but supports 10bit addresses (up to 1023 node IDs)
-#define NETWORKID             1
+#define NODEID                116 // must be unique for each node on same network (supports 10bit addresses (up to 1023 node IDs))
+#define NETWORKID             110
+#define ROOM_GATEWAYID        110
 #define GATEWAYID             1
 #define GATEWAY_NETWORKID     1
 #define ENCRYPTKEY            "Tt-Mh=SQ#dn#JY3_"
@@ -141,20 +141,15 @@ void sendData()
   readSensors();
   
   // send datapacket
-  //radio.sendWithRetry(ROOM_GATEWAYID, dataPacket, strlen(dataPacket));  // send data
-  //WDT_sleep();   // sleep 4 seconds before sending data to main gateway
-  //radio.setNetwork(GATEWAY_NETWORKID);
+  radio.sendWithRetry(ROOM_GATEWAYID, dataPacket, strlen(dataPacket));  // send data
+  WDT_sleep();   // sleep 4 seconds before sending data to main gateway
+  radio.setNetwork(GATEWAY_NETWORKID);
   radio.sendWithRetry(GATEWAYID, dataPacket, strlen(dataPacket));
-  //radio.setNetwork(NETWORKID);
+  radio.setNetwork(NETWORKID);
 
   memset(dataPacket, 0, sizeof dataPacket);   // clear array
-  //_dataPacket[0] = (char)0;
 
-  digitalWrite(LED, HIGH);
-  delay(5);
-  digitalWrite(LED, LOW);
-  //fadeLED();
-  
+  blinkLED(LED);
 }
 
 
@@ -170,12 +165,10 @@ void readSensors()
   float batt = (adc_a7/1023) * 2 * 3.3;
   
   // define character arrays for all variables
-  char _i[3];
   char _d[3];
   char _b[5];
   
   // convert all flaoting point and integer variables into character arrays
-  dtostrf(NODEID, 1, 0, _i);
   dtostrf(d, 1, 0, _d);  // this function converts float into char array. 3 is minimum width, 2 is decimal precision
   dtostrf(batt, 4, 2, _b);
   delay(5);
@@ -183,9 +176,7 @@ void readSensors()
   dataPacket[0] = 0;  // first value of dataPacket should be a 0
   
   // create datapacket by combining all character arrays into a large character array
-  strcat(dataPacket, "i:");
-  strcat(dataPacket, _i);
-  strcat(dataPacket, ",d:");
+  strcat(dataPacket, "d:");
   strcat(dataPacket, _d);
   strcat(dataPacket, ",b:");
   strcat(dataPacket, _b);
@@ -217,6 +208,16 @@ float averageADC(int pin)
   float average = sum/5.0;
   return average;
 }
+
+
+// blink LED *****************************************
+void blinkLED(int pin)
+{
+  digitalWrite(pin, HIGH);
+  delay(5);
+  digitalWrite(pin, LOW);
+}
+
 
 // fade LED **********************************************************************
 void fadeLED()
